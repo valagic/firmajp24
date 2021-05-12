392576a121388567e87bbd98d6fd21797f85b2bd
Vedran Alagić <vedran.alagic@outlook.com>
2021-05-12 17:42:51 +0200

    prva tablica

--- firmajp24/firmajp24.sql
+++ firmajp24/firmajp24.sql
@@ -2,7 +2,9 @@ drop database if exists firmajp24;
 create database firmajp24;
 use firmajp24;
 
-create table novo (
+create table projekt (
 	id int not null primary key auto_increment,
-	naziv varchar(20) not null
+	naziv varchar(20) not null,
+	cijena decimal(18,2) not null
 );
+
