259e1220cadd82d52c06759d70335db82882bb99
Vedran Alagić <vedran.alagic@outlook.com>
2021-05-12 17:40:24 +0200

    cetvrti commit

--- firmajp24/firmajp24.sql
+++ firmajp24/firmajp24.sql
@@ -3,5 +3,6 @@ create database firmajp24;
 use firmajp24;
 
 create table novo (
-	id int not null primary key auto_increment
+	id int not null primary key auto_increment,
+	naziv varchar(20) not null
 );
